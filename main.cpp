4dc5cf648e15b946c102bc38330bfb94dc408a11
Edward Atkinson <edat444@gmail.com>
2017-07-25 17:55:20 -0400

    will this work?

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+string help();
 
 int main()
 {
+    cout << "AHHH HELP ME!" << help();
     return 0;
+}
+
+string help()
+{
+  return "help!";
 }
